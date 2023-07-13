f7673b282c5f9107327d6cb4ffcb38f0bdfdd5f2
uuronn <uuronprogram@gmail.com>
2023-07-14 01:05:02 +0900

    Userをグローバルに取得できるようにしました

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,15 +1,11 @@
 import { Button } from "~/components/Button";
-import { VerifyFirebaseAuthGuard } from "~/guards/VerifyFirebaseAuthGuard";
 import { useLogin } from "./hooks/useLogin";
-
 const LoginPage = () => {
   return (
-    <VerifyFirebaseAuthGuard>
-      <main>
-        <div>ログインしてください。</div>
-        <Button onClick={useLogin}>Googleでログイン</Button>
-      </main>
-    </VerifyFirebaseAuthGuard>
+    <main>
+      <div>ログインしてください。</div>
+      <Button onClick={useLogin}>Googleでログイン</Button>
+    </main>
   );
 };
 
