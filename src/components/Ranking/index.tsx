44c54a87bb4db78c4e8765945ae565ee4bd35b81
takuaki <matsutakuaki@gmail.com>
2023-07-16 02:59:05 +0900

    レスポンシブ対応したよ

--- src/components/Ranking/index.tsx
+++ src/components/Ranking/index.tsx
@@ -9,23 +9,32 @@ const RankingNameCSS = css`
   background-color: #ddd;
   font-size: 40px;
   padding: 5px;
-  width: 65%;
-  display: inline-block;
+  width: 100%;
   margin: 10px;
   text-align: center;
   font-weight: bold;
+  @media (max-width:500px) {
+    font-size: 20px;
+    width: 60%;
+    margin: 5px;
+  }
 `;
 const pointCSS = css`
   background-color: #eee;
   font-size: 40px;
   padding: 5px;
   margin: 10px;
-  width: 30%;
-  display: inline-block;
+  width: 100%;
   text-align: center;
+  @media (max-width:500px) {
+    font-size: 20px;
+    width: 30%;
+    margin: 5px;
+  }
 `;
 const Ranking = css`
-display: flex;
+  display: flex;
+  width: 100%;
 `;
 
 export const RankingData = ({ RankingName, point, ...props }: RankingProps) => {
