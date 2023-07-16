7f9bcac1fce36579c97c429130fc5aa29b6bf458
uuronn <uuronprogram@gmail.com>
2023-07-16 17:35:55 +0900

    リファクタリングしてとりあえず動いている

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -3,6 +3,8 @@ import { useGoogleLogin } from "./hooks/useGoogleLogin";
 import { useLogout } from "./hooks/useLogout";
 import { css } from "@emotion/react";
 import googleImg from "../../../assets/google.svg";
+import { useAuthContext } from "~/context/hooks/useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 const LoginBox = css`
   flex-direction: column;
@@ -10,11 +12,13 @@ const LoginBox = css`
   display: flex;
   border: 3px solid #000000;
   border-radius: 20px;
-  @media (max-width:500px){
+
+  @media (max-width: 500px) {
     padding: 10px;
     border: none;
   }
 `;
+
 const Main = css`
   height: 100vh;
   display: flex;
@@ -31,11 +35,21 @@ const contents = css`
   height: 80px;
   width: 300px;
 `;
+
 const img = css`
   width: 25px;
 `;
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const { user } = useAuthContext();
+
+  if (user) {
+    navigate("/");
+
+    return <div>loading...</div>;
+  }
+
   return (
     <main css={Main}>
       <div css={LoginBox}>
