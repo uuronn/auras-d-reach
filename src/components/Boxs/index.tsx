d8ef4d0c85189b26c74301f506d9fb0c9dc1f360
takuaki <matsutakuaki@gmail.com>
2023-07-17 10:12:45 +0900

    thee.jsの修正

--- src/components/Boxs/index.tsx
+++ src/components/Boxs/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import * as THREE from "three";
-import King from "../../assets/king.png";
+import Earth from "../../assets/earth2.jpg";
 function Box() {
   useEffect(() => {
     //scene
@@ -8,23 +8,39 @@ function Box() {
 
     //camera
     const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
-      45,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
+      40,
+      window.innerWidth / window.innerHeight
     );
+
     //renderer
-    const renderer: THREE.WebGL1Renderer = new THREE.WebGL1Renderer();
+    const renderer: THREE.WebGL1Renderer = new THREE.WebGL1Renderer({
+      antialias: true
+    });
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
     //boxsize
-    const geometry = new THREE.BoxGeometry(2, 2, 2);
-    const texture = new THREE.TextureLoader().load(King); 
-    const material = new THREE.MeshBasicMaterial({ map:texture });
-    // const material = new THREE.MeshBasicMaterial( { map:texture } );
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    const geometry = new THREE.SphereGeometry(300, 30, 390);
+    const material = new THREE.MeshStandardMaterial({
+      map: new THREE.TextureLoader().load(Earth)
+    });
+    const earth = new THREE.Mesh(geometry, material);
+    scene.add(earth);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    directionalLight.position.set(1, 1, 1);
+    scene.add(directionalLight);
+
+    const pointLight = new THREE.PointLight(0xffffff, 2, 1000);
+    scene.add(pointLight);
+    const pointLightHelper = new THREE.PointLightHelper(pointLight, 30);
+    scene.add(pointLightHelper);
+    // const geometry = new THREE.BoxGeometry(2, 2, 2);
+    // const texture = new THREE.TextureLoader().load(Earth);
+    // const material = new THREE.MeshBasicMaterial({ map:texture });
+    // // const material = new THREE.MeshBasicMaterial( { map:texture } );
+    // const cube = new THREE.Mesh(geometry, material);
+    // scene.add(cube);
 
     //3dmodel
     // const gltfLoader = new GLTFLoader();
@@ -33,15 +49,52 @@ function Box() {
     //     scene.add(model);
     // });
 
-    camera.position.z = 5;
+    // camera.position.z = 5;
 
-    //animation
-    function animate() {
-      requestAnimationFrame(animate);
+    // //animation
+    // function animate() {
+    //   requestAnimationFrame(animate);
 
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.008;
+    // //   cube.rotation.x += 0.01;
+    // //   cube.rotation.y += 0.008;
+    //   renderer.render(scene, camera);
+
+    function createStarField() {
+      const vertices = [];
+      for (let i = 0; i < 500; i++) {
+        const x = 3000 * (Math.random() - 0.5);
+        const y = 3000 * (Math.random() - 0.5);
+        const z = 3000 * (Math.random() - 0.5);
+        vertices.push(x, y, z);
+      }
+      const geometry = new THREE.BufferGeometry();
+      geometry.setAttribute(
+        "position",
+        new THREE.Float32BufferAttribute(vertices, 3)
+      );
+
+      const material = new THREE.PointsMaterial({ size: 8, color: 0xffffff });
+
+      const stars = new THREE.Points(geometry,material);
+      scene.add(stars);
+    }
+
+    createStarField();
+
+    let rot = 0;
+    function tick() {
+      rot += 0.5;
+      const radian = (rot * Math.PI) / 180;
+      camera.position.x = 1000 * Math.sin(radian);
+      camera.position.y = 1000 * Math.sin(radian);
+      camera.lookAt(new THREE.Vector3(0, 0, -400));
+      pointLight.position.set(
+        500 * Math.sin(Date.now() / 500),
+        500 * Math.sin(Date.now() / 1000),
+        500 * Math.cos(Date.now() / 500)
+      );
       renderer.render(scene, camera);
+      requestAnimationFrame(tick);
     }
 
     //windowSizeKeep
@@ -53,7 +106,8 @@ function Box() {
 
     window.addEventListener("resize", onWindowResize);
 
-    animate();
+    // animate();
+    tick();
   }, []);
   return <div></div>;
 }
