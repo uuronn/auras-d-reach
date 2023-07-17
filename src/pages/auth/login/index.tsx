6f8faf99d381924414851af46606d2fe8be52799
takuaki <matsutakuaki@gmail.com>
2023-07-17 11:14:38 +0900

    three.jsを背景に設定

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -4,6 +4,7 @@ import { css } from "@emotion/react";
 import googleImg from "../../../assets/google.svg";
 import { useAuthContext } from "~/context/hooks/useAuthContext";
 import { useNavigate } from "react-router-dom";
+import Box from "~/components/Boxs";
 
 const LoginBox = css`
   flex-direction: column;
@@ -11,6 +12,8 @@ const LoginBox = css`
   display: flex;
   border: 3px solid #000000;
   border-radius: 20px;
+  background-color: #000000;
+  opacity: 0.7;
 
   @media (max-width: 500px) {
     padding: 10px;
@@ -23,6 +26,10 @@ const Main = css`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
 `;
 
 const contents = css`
@@ -33,11 +40,18 @@ const contents = css`
   font-weight: bolder;
   height: 80px;
   width: 300px;
+  &:hover{
+    opacity: 0.8;
+  }
 `;
 
 const img = css`
   width: 25px;
 `;
+
+// const abs = css`
+//   position: absolute;
+// `
 
 const LoginPage = () => {
   const navigate = useNavigate();
@@ -50,14 +64,17 @@ const LoginPage = () => {
   }
 
   return (
-    <main css={Main}>
-      <div css={LoginBox}>
-        <Button css={contents} onClick={useGoogleLogin}>
-          <img css={img} src={googleImg} />
-          Googleでログイン
-        </Button>
-      </div>
-    </main>
+    <div>
+      <Box/>
+      <main css={Main}>
+        <div css={LoginBox}>
+          <Button css={contents} onClick={useGoogleLogin}>
+            <img css={img} src={googleImg} />
+            Googleでログイン
+          </Button>
+        </div>
+      </main>
+    </div>
   );
 };
 
